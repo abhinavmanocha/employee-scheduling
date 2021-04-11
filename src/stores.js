865ae9369b7e17bb08ceb8abac06f15263ea13af
Jakub Kupczyk <jkbkupczyk@gmail.com>
2021-04-12 01:12:44 +0200

    remove files

--- src/stores.js
+++
@@ -1,6 +0,0 @@
-import { writable } from "svelte/store";
-
-export const user = writable(localStorage.getItem('user'));
-export const token = writable(localStorage.getItem('token'));
-export const jwt = writable(localStorage.getItem('jwt'));
-export const loggedUser = writable(localStorage.getItem('loggedUser'));
